9c4dfae634837bd63f6c4cbc500f6ecac0b41341
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-16 17:23:35 -0500

    try using axios directly with interceptor for auth header

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import router from '@/router';
 import store from '@/store';
 import axios from 'axios';
 
-Vue.prototype.$api = axios.create();
-
 import { Auth0Plugin } from "./auth";
 
 async function main() {
 
-  let resp = await this.$api.get('/api/auth0-secrets');
+  let resp = await axios.get('/api/auth0-secrets');
   let secrets = resp.data;
   let domain = secrets.AUTH0_DOMAIN;
   let clientId = secrets.AUTH0_CLIENT_ID;
