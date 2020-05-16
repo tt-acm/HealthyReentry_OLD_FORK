511bce41523cc7e114b9e7aeb81676913cadc22c
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-15 21:03:56 -0500

    update readme to show correct commands for dev and prod

--- README.md
+++ README.md
@@ -7,12 +7,17 @@ npm install
 
 ### Compiles and hot-reloads for development
 ```
-npm run serve
+npm run dev
 ```
 
 ### Compiles and minifies for production
 ```
 npm run build
+```
+
+### Start production server
+```
+npm start
 ```
 
 ### Lints and fixes files
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,8 @@ const router = require('express').Router();
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
 
-  
+
+
 // these aren't really secrets
 router.get('/api/auth0-secrets', function(req, res) {
   const creds = {
